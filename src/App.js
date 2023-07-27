a9999c9d7da83cdaf57bce46287f0cb25e066e4a
rishabh0111 <rishabhsharma8912@gmail.com>
2023-07-27 11:28:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useState } from 'react';
+import ImageUploader from './components/ImageUploader';
+import MessageInput from './components/MessageInput';
+import Steganography from './components/Steganography';
+import ImagePreview from './components/ImagePreview';
+import Encryption from './components/Encryption';
+import Decoding from './components/Decoding';
 import './App.css';
 
 function App() {
+  const [imageFile, setImageFile] = useState(null);
+  const [secretMessage, setSecretMessage] = useState('');
+  const [encodedImage, setEncodedImage] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Image Steganography App</h1>
+      <ImageUploader setImageFile={setImageFile} />
+      {imageFile && <ImagePreview imageFile={imageFile} />}
+      <MessageInput setSecretMessage={setSecretMessage} />
+      <Steganography
+        imageFile={imageFile}
+        secretMessage={secretMessage}
+        setEncodedImage={setEncodedImage}
+      />
+      {encodedImage && <ImagePreview imageFile={encodedImage} />}
+      <Encryption encodedImage={encodedImage} setEncodedImage={setEncodedImage} />
+      <Decoding encodedImage={encodedImage} />
     </div>
   );
 }
